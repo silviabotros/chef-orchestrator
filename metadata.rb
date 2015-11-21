13b4d1371cdf5c9eb782489f920ff243289140b0
sgopsbot <operations+hubot@sendgrid.com>
2015-11-20 21:17:44 -0800

    add in version function

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,16 @@
+if File.file?("version_wf")
+  version_var = IO.read(File.join(File.dirname(__FILE__), 'version_wf'))
+else
+  version_var = "0.0.0"
+end
+
 name 'orchestrator'
 maintainer 'Silvia Botros'
 maintainer_email 'silvia.botros@sendgrid.com'
 license 'All rights reserved'
 description 'Installs/Configures chef-orchestrator'
 long_description 'Installs/Configures chef-orchestrator'
-version IO.read(File.join(File.dirname(__FILE__), 'version_wf')) || "testing"
+version version_var
 
 depends 'yum'
 depends 'packagecloud'
@@ -13,3 +19,4 @@ depends 'percona', '~> 0.15.0'
 
 supports 'centos'
 supports 'ubuntu'
+
